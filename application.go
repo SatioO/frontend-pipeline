7fc4af04595e6736d0638a66b0634e859d6940e3
SatioO <vaibhav.satam29991@gmail.com>
2020-11-10 15:03:21 +0530

    added few more configs

--- application.go
+++ application.go
@@ -2,36 +2,68 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func kafkaConsumer(config *kafka.ConfigMap, topics []string) {
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
 	c, err := kafka.NewConsumer(config)
 	defer c.Close()
 
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Failed to create consumer: %s\n", err)
+		os.Exit(1)
 	}
+
+	fmt.Printf("Created Consumer %v\n", c)
 
 	c.SubscribeTopics(topics, nil)
 
-	for {
-		msg, err := c.ReadMessage(-1)
-		if err == nil {
-			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-		} else {
-			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+	run := true
+
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := c.Poll(100)
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("%% Message on %s:\n%s\n",
+					e.TopicPartition, string(e.Value))
+			case kafka.Error:
+				// Errors should generally be considered as informational, the client will try to automatically recover
+				fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+			}
 		}
 	}
 }
 
 func main() {
+	if len(os.Args) < 4 {
+		fmt.Fprintf(os.Stderr, "Usage: main.go <broker> <group> <topics..>\n")
+		os.Exit(1)
+	}
+
+	broker := os.Args[1]
+	group := os.Args[2]
+	topics := os.Args[3:]
+
 	configMap := &kafka.ConfigMap{
-		"bootstrap.servers": "localhost:9092",
-		"group.id":          "myGroup",
+		"bootstrap.servers":  broker,
+		"group.id":           group,
+		"session.timeout.ms": 6000,
+		// "go.application.rebalance.enable": true,
+		// "go.events.channel.enable":        true,
 		"auto.offset.reset": "earliest",
 	}
 
-	kafkaConsumer(configMap, []string{"first_topic"})
+	kafkaConsumer(configMap, topics)
 }
