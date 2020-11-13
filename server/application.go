9e0584807917e3fcbc8aca8f2d9ec55629f1f35c
SatioO <vaibhav.satam29991@gmail.com>
2020-11-13 17:02:06 +0530

    added mongo

--- server/application.go
+++ server/application.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "I am Root")
+	fmt.Println("FOO:", os.Getenv("FOO"))
+	fmt.Println("BAR:", os.Getenv("BAR"))
+	fmt.Fprintln(w, "I am "+os.Getenv("FOO"))
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +24,29 @@ func greet(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	ConnectDB("users")
+
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/hello", greet)
 	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+// ConnectDB ...
+func ConnectDB(dbName string) *mongo.Database {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
+	client, err := mongo.NewClient(clientOptions)
+
+	err = client.Connect(ctx)
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal("Couldn't connect to the database", err)
+	} else {
+		log.Println("Connected!")
+	}
+
+	return client.Database(dbName)
 }
--- server/go.mod
+++ server/go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/confluentinc/confluent-kafka-go v1.5.2
+	go.mongodb.org/mongo-driver v1.4.3
 	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.5.2
 )
