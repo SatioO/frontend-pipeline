48f8e6c21d5b80cfe87f21d05c497f96c4643cc3
SatioO <vaibhav.satam29991@gmail.com>
2020-11-14 12:59:35 +0530

    added diwali wishes

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Wishing you a very happy and prosperous Diwali! 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/satioO"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          From: Vaibhav Satam
         </a>
       </header>
     </div>
