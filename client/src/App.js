3bc77b7889c5ae9dbe128212f71d911233d1f7a1
SatioO <vaibhav.satam29991@gmail.com>
2020-11-14 14:08:56 +0530

    fixed mongo config

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          From: Vaibhav Satam
+          Vaibhav Satam
         </a>
       </header>
     </div>
