7639949d68a35caeccdbf3a321b474d9b43bb109
SatioO <vaibhav.satam29991@gmail.com>
2020-11-15 21:58:20 +0530

    added disabled condition for pagination buttons

--- client/src/modules/Friends/components/Footer.js
+++ client/src/modules/Friends/components/Footer.js
@@ -3,20 +3,33 @@ import React from "react"
 function Footer(props) {
     return (
         <div className="footer">
-            <img
+            {props.page.offset !== 0 ? <img
                 src={process.env.PUBLIC_URL + '/icons/keyboard_arrow_left-24px.svg'}
                 alt="search"
                 className="icon"
                 onClick={props.onPreviousPage}
-                style={{ pointerEvents: props.page.offset === 0 ? "none": "initial" }}
-            />
-            <img
-                src={process.env.PUBLIC_URL + '/icons/keyboard_arrow_right-24px.svg'}
-                alt="add"
-                className="icon"
-                onClick={props.onNextPage}
-                style={{ pointerEvents: (props.page.total <= props.page.offset + 4) ? "none": "initial" }}
-            />
+            /> : <img
+                    src={process.env.PUBLIC_URL + '/icons/keyboard_arrow_left_disabled-24px.svg'}
+                    alt="search"
+                    className="icon"
+                />
+            }
+
+            {props.page.total > props.page.offset + 4
+                ? <img
+                    src={process.env.PUBLIC_URL + '/icons/keyboard_arrow_right-24px.svg'}
+                    alt="add"
+                    className="icon"
+                    onClick={props.onNextPage}
+                />
+                : <img
+                    src={process.env.PUBLIC_URL + '/icons/keyboard_arrow_right_disabled-24px.svg'}
+                    alt="add"
+                    className="icon"
+
+                />
+            }
+
         </div>
     )
 }
